50174c477361a4f4f96b95c68622de4b813657cd
David Charte <fdavidcl@outlook.com>
2016-07-15 22:53:27 +0200

    Parallel script

--- exe.rb
+++ exe.rb
@@ -1,7 +1,11 @@
 #!/usr/bin/env ruby
 
 ARGV.each do |name|
-  (0 ... 25).each do |n|
-    `bin/ans #{name} out/results_#{name}#{n}.csv out/diversity_#{name}#{n}.csv`
+  fork do
+    (0 ... 25).each do |n|
+      `bin/ans #{name} out/results_#{name}#{n}.csv out/diversity_#{name}#{n}.csv`
+    end
   end
 end
+
+Process.waitall
