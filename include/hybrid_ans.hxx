9cbbadfb90cca16f9a3185da3c692edf2bb158dd
David Charte <fdavidcl@outlook.com>
2016-07-15 22:53:15 +0200

    Max local ratio should be decimal

--- include/hybrid_ans.hxx
+++ include/hybrid_ans.hxx
@@ -11,11 +11,11 @@ protected:
 
   realea::ProblemCEC2014 cec2014;
   realea::ProblemPtr problem;
-  unsigned evaluations_ls,
-    max_local_ratio;
+  unsigned evaluations_ls;
+  double max_local_ratio;
 
 public:
-  HANS(unsigned m, unsigned c, unsigned n, unsigned d, unsigned f, double sigma, double min, double max, realea::ILocalSearch* ls, unsigned mlr)
+  HANS(unsigned m, unsigned c, unsigned n, unsigned d, unsigned f, double sigma, double min, double max, realea::ILocalSearch* ls, double mlr)
     :ANSBase(m, c, n, d, f, sigma, min, max), ls(ls), evaluations_ls(0), max_local_ratio(mlr),
     cec2014(d), problem(cec2014.get(f)) {}
 
